70fed24bae9aea378effe72941b3fcc1431891cf
Agustin Barto <abarto@gmail.com>
2016-09-10 09:16:43 -0300

    Add result templates

--- django_watermark_images/django_watermark_images/urls.py
+++ django_watermark_images/django_watermark_images/urls.py
@@ -18,12 +18,16 @@ from django.core.urlresolvers import reverse_lazy
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 
-from items.views import text_overlay, watermark, steganography
+from items.views import (text_overlay, watermark, steganography, text_overlay_result, watermark_result,
+                         steganography_result)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', RedirectView.as_view(url=reverse_lazy('text_overlay')), name='home'),
-    url(r'^text_overlay', text_overlay, name='text_overlay'),
-    url(r'^watermark', watermark, name='watermark'),
-    url(r'^steganography', steganography, name='steganography')
+    url(r'^text_overlay/$', text_overlay, name='text_overlay'),
+    url(r'^text_overlay_result/(?P<result_id>[0-9a-f]{32})/$', text_overlay_result, name='text_overlay_result'),
+    url(r'^watermark/$', watermark, name='watermark'),
+    url(r'^watermark_result/(?P<result_id>[0-9a-f]{32})/$', watermark_result, name='watermark_result'),
+    url(r'^steganography/$', steganography, name='steganography'),
+    url(r'^steganography_result/(?P<result_id>[0-9a-f]{32})/$', steganography_result, name='steganography_result')
 ]
